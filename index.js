0ef67bde61befb0e3212ba82828ef2f0e43d9782
Fernando <feflores21400@gmail.com>
2022-06-10 20:43:01 -0400

    commit tree eb4d0f29c9dc10509f950da65a8c946e1caf361b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`   ;
+};
+saturdayFun();
+function mondayWork(_activity = 'go to the office') {
+    return `This Monday, I will ${_activity}.`;};
+mondayWork();
+function wrapAdjective(adjective = 'Great'){
+    let result = wrapAdjective('*');
+    let emphatic = result('a hard worker')
+    {
+        return `You are ${emphatic}!`
+    };}
+    
+  
